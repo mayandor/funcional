d32c761a8aadff2a1bd104ca4d1df41012cf5e99
mayandor <mayapologies6@gmail.com>
2020-12-15 00:06:28 -0400

    estilos

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,30 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/sidebar.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <div class="sidenav" style="background-color:<?php echo $_SESSION['color']; ?>">
-        <a>SEA</a>
-        <a href="#"><img src="./img/birrete.png" alt="" width="60"></a>
+    <div class="sidenav">
+        <h1 class="titulo">SEA</h1>
+        <img src="./img/birrete.png" alt="" width="60">
         <?php
             if($_SESSION['rol']=='e'){
         ?>
+        <div id="menu">
         <ul>
-            <li><a href="#" class="menuDesplegable">Curso de Verano</a>
+            <li class="parent">
+                <a href="#"><span class="material-icons">layers</span>     Curso de Verano</a>
                 <ul>
-                    <li class="menuDesplegable"><a href="inscripcion.php">Inscripcion</a></li>
-                    <li class="menuDesplegable"><a href="#">Materias inscritas</a></li>
+                    <li><a href="inscripcion.php">Inscripcion</a></li>
+                    <li><a href="#">Materias inscritas</a></li>
                 </ul>
-            </li>    
+            </li> 
         </ul>
-            
+        </div>
         <?php
         }
-            if($_SESSION['rol']=='d'){
+            if($_SESSION['rol']=='a'){
         ?>
-            <a href="#" class="menu">Listar Est</a>
-            <a href="#">Reporte</a>
+        <div id="menu">
+            <ul>
+                <li><a href="#"><span class="material-icons">folder_open</span>   Listar Estudiantes</a></li>
+                <li><a href="#"><span class="material-icons">text_snippet</span>   Reporte</a></li>
+            </ul> 
+        </div>
         <?php
         }
         ?>
